154be741fac72796d5de467d8870847bead3c390
Gabor Heja <gheja@m002560p>
2018-08-24 12:56:08 +0200

    Now adding multiple instances of the same object

--- src/main.js
+++ src/main.js
@@ -8,42 +8,77 @@ let camera;
 let sphere;
 let n = 0;
 
+let _a;
+let _material;
+let _loader;
+let _box;
+let _boxes;
+let _shadowGenerator;
+let _light;
+
 function createScene()
 {
 	// Create scene
 	let scene = new BABYLON.Scene(engine);
+	let plane;
 	
-	// Create simple sphere
-	sphere = BABYLON.Mesh.CreateIcoSphere("sphere", {radius:0.2, flat:true, subdivisions: 1}, scene);
-	sphere.position.y = 3;
-	sphere.material = new BABYLON.StandardMaterial("sphere material", scene)
+	_light = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(0, 0, -30), scene);
+	_light.intensity = 0.5;
 	
-	// Lights and camera
-	let light = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(0, -0.5, 1.0), scene);
-	light.position = new BABYLON.Vector3(0, 5, -2);
+	camera = new BABYLON.FreeCamera("sceneCamera", new BABYLON.Vector3(0, 5, -15), scene);
+	camera.attachControl(canvas, true);
 	
-	camera = new BABYLON.FreeCamera("sceneCamera", new BABYLON.Vector3(0, 3, -5), scene);
-	camera.setTarget(new BABYLON.Vector3(0, 0, 0));
-//	camera.attachControl(canvas, true);
+	let mat2 = new BABYLON.StandardMaterial("sa", scene);
+	mat2.diffuseColor = new BABYLON.Color3(1, 0.8, 0.3);
 	
-	// Default Environment
-	let environment = scene.createDefaultEnvironment({ enableGroundShadow: true, groundYBias: 1 });
-	environment.setMainColor(BABYLON.Color3.FromHexString("#223344"));
+	_material = new BABYLON.StandardMaterial("sphere material", scene);
+//	_material.alpha = 0.5;
+	_material.backFaceCulling = true;
 	
-	// Shadows
-	let shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
-	shadowGenerator.useBlurExponentialShadowMap = true;
-	shadowGenerator.blurKernel = 32;
-	shadowGenerator.addShadowCaster(sphere, true);
+	_loader = new BABYLON.AssetsManager(scene);
+	_box = _loader.addMeshTask("box", "", "./", "icosahedron.obj");
+	_loader.onFinish = loaderFinished;
+	_loader.load();
+	
+	_shadowGenerator = new BABYLON.ShadowGenerator(1024, _light);
+	_shadowGenerator.useBlurExponentialShadowMap = true;
+	_shadowGenerator.blurKernel = 32;
+	
+	plane = BABYLON.Mesh.CreatePlane('impostor', 100, scene);
+	plane.position.z = 50;
+	plane.receiveShadows = true;
+	plane.material = mat2;
+	
+	scene.onBeforeRenderObservable.add(onUpdate);
 	
 	// Enable VR
 	let vrHelper = scene.createDefaultVRExperience({ createDeviceOrientationCamera:true });
 	
-	// Runs every frame to rotate the sphere
-	scene.onBeforeRenderObservable.add(onUpdate);
-	
 	return scene;
-};
+}
+
+function loaderFinished()
+{
+	let i, a, base;
+	
+	_boxes = [];
+	
+	base = _box.loadedMeshes[0];
+	base.material = _material;
+	base.setEnabled(false);
+	base.scaling.x = 0.3;
+	base.scaling.y = 0.3;
+	base.scaling.z = 0.3;
+	_shadowGenerator.addShadowCaster(base, true);
+	
+	for (i=0; i<15; i++)
+	{
+		a = base.createInstance();
+		_shadowGenerator.addShadowCaster(a, true);
+		_boxes.push(a);
+	}
+	
+}
 
 function init()
 {
@@ -81,23 +116,23 @@ function onResize()
 	engine.resize();
 }
 
-function log2()
-{
-	console.log("-");
-	console.log(camera.cameraDirection);
-	console.log(camera.cameraRotation);
-}
-
 function onUpdate()
 {
-	n++;
-	scene.activeCamera.position.y = 5 + Math.sin(n / 1000) * 2;
-	// scene.activeCamera.position.z = -15 + Math.sin(n / 3) * 5;
+	let i;
 	
-	sphere.rotation.y += 0.0005 * scene.getEngine().getDeltaTime();
-	sphere.rotation.x += 0.0005 * scene.getEngine().getDeltaTime();
+	n += 0.1;
+	
+	if (_boxes)
+	{
+		for (i=0; i<_boxes.length; i++)
+		{
+			_boxes[i].position.x = Math.sin((n * 0.3 + i) / 10) * 5;
+			_boxes[i].position.y = Math.sin((n * 0.3 + i) / 15) * 5;
+			_boxes[i].position.z = Math.sin((n * 0.3 + i) / 20) * 5;
+			_boxes[i].rotation.z = Math.sin((n * 0.3 + i) / 5) * 5;
+		}
+	}
 }
 
 window.addEventListener("resize", onResize);
 window.addEventListener("load", init);
-window.setInterval(log2, 1000);
